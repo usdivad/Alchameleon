3fe71d6dc6b4f2b2859db8e7c63acdfdeeb0c347
David Su <david.d.su@gmail.com>
2015-05-04 18:19:13 -0400

    start searching

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Initial vars for Express app and server
+var http = require('http');
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = http.createServer(app);
 var port = 8000;
 
 // AlchemyAPI
@@ -18,7 +19,44 @@ server.listen(port, function() {
 
 // Homepage
 app.use(express.static('public'));
-app.get('/', function(req, res) {
-    // res.send('THUG LYFE');
-    res.send('index.html');
-});
+app.get('/', home);
+
+
+// Functions
+function home(req, res) {
+    res.send('THUG LYFE');
+    console.log('hi');
+    get_wiki_search('hollywood');
+    // res.send('no.html');
+}
+
+function entities(req, res, output) {
+    alchemyapi.entities('url')
+}
+
+function get_wiki_url(query) {
+    var search_results = get_wiki_search(query);
+    // console.log(search_results);
+    url = search_results[2];
+    // console.log(url);
+}
+function get_wiki_search(query) {
+    console.log('searching for ' + query);
+    var options = {
+        host: 'en.wikipedia.org',
+        path: '/w/api.php?action=opensearch&format=json&search=' + query
+    };
+    var results_str = ''
+    callback = function(res) {
+        res.on('data', function(chunk) {
+            results_str += chunk;
+        });
+        res.on('end', function() {
+            console.log('done!');
+            // console.log(results_str);
+            return JSON.parse(results_str);
+        })
+    };
+    
+    http.request(options, callback).end();
+}
